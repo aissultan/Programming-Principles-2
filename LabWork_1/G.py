41859b95a3d2d11ccdfb0615b37d5d158621a8b4
Aisultan Khalelov <a_khalelov@kbtu.kz>
2022-02-09 22:58:57 +0500

    Lab 1

--- LabWork_1/G.py
+++ LabWork_1/G.py
@@ -1,9 +1,28 @@
-binarynum = input()
+binar = input()
 
-decimalnum = 0
+a = len(binar) - 1 
 
-for digit in binarynum:
-    decimalnum = decimalnum*2 + int(digit)
-print(decimalnum)
+res = 0
 
+def toDec(binar, cnt):
+    global res
+    if cnt == a + 1:
+        return res
+
+    res += int(int(binar[cnt]) * (2 **(a - cnt)))
+    return toDec(binar, cnt + 1)
+
+toDec(binar, 0)
+print(res)
+
+# without recursion 
+# _________________________________________
+# binarynum = input()
+
+# decimalnum = 0
+
+# for digit in binarynum:
+#     decimalnum = decimalnum*2 + int(digit)
+# print(decimalnum)
+# _________________________________________
 # print(int(binarynum, 2)) - another method
--- LabWork_1/J.py
+++ LabWork_1/J.py
@@ -1,25 +1,5 @@
-s = input()
+a = input().split()
 
-word = ""
-result = ""
-
-def getLen(t):
-    cnt = 0
-    for i in t:
-        cnt = cnt + 1
-    return cnt
-
-for i in range(len(s)):
-    if(s[i] != " "):
-        word = word + s[i]
-
-    if(s[i] == " "):
-        if(getLen(word) >= 3):
-            result = result + word + " "
-        word = ""
-
-    if(i == len(s) - 1):
-        if(getLen(word) >= 3):
-            result = result + word
-
-print(result)
+for i in a:
+    if len(i) >= 3:
+        print(i, end = " ")
